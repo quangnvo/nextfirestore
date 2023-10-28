8efbdedfe6890922fcb593bc57910498c6668ca8
quangnvo <voquang.usth@gmail.com>
2023-10-28 22:45:47 +0800

    create nextfirestore

---
+++ app/firebase.ts
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from "firebase/firestore";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAq6_9R10gwC2WWOn2MrMmSUWmwMP3X2xg",
+  authDomain: "expense-tracker-9828f.firebaseapp.com",
+  projectId: "expense-tracker-9828f",
+  storageBucket: "expense-tracker-9828f.appspot.com",
+  messagingSenderId: "548994979630",
+  appId: "1:548994979630:web:dd2a92bc86143c15dc4cbf",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
